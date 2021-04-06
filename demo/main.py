1da975bb929ce7f2fc4ef68c6eeb075948ef9e07
yinghongbin <yinghongbin@192.168.0.102>
2021-04-07 01:03:39 +0800

    123

--- demo/main.py
+++ demo/main.py
@@ -1,10 +1,16 @@
 import pyautogui
 # document url https://pyautogui.readthedocs.io/en/latest/quickstart.html
-print(pyautogui.position())# current mouse x and y
-print(pyautogui.size())# current screen resolution width and height
-print(pyautogui.onScreen(10000, 20)) # True if x & y are within the screen.
-pyautogui.FAILSAFE = True
-pyautogui.PAUSE = 2.5
-pyautogui.moveTo(10, 10, duration=2)
+# TK_SILENCE_DEPRECATION=1
+# print(pyautogui.position())# current mouse x and y
+# print(pyautogui.size())# current screen resolution width and height
+# print(pyautogui.onScreen(10000, 20)) # True if x & y are within the screen.
+# pyautogui.FAILSAFE = True
+# pyautogui.PAUSE = 1
+# pyautogui.moveTo(10, 10,0)
 
-pyautogui.moveRel(20, 20, duration=2)
+# pyautogui.moveRel(20,200,0)
+# pyautogui.dragRel(20, 20,1,button='left')  # drag mouse to XY
+# pyautogui.scroll(10)
+# pyautogui.alert(text='123', title='123', button='OK')
+button7location = pyautogui.locateOnScreen('1231.png', grayscale=False,confidence=0.4)
+print(button7location)
